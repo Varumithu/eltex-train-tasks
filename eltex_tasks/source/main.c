8e439b852c81dfc4d31a58c5186df8ebdf61059c
valmit <sliper98@gmail.com>
2020-11-28 16:51:16 +0700

    improved matrix printing for negative numbers

--- eltex_tasks/source/main.c
+++ eltex_tasks/source/main.c
@@ -7,15 +7,18 @@
 #include "print_matrix.h"
 #include "diagonal_matrix.h"
 
-int main()
-{
+void test_print_with_negatievs() {
+    int array[] = {10, 100, 90, -101, -505, 780, 1, 2, 3};
+    print_matrix_int(&array[0], 3, 3);
+}
+
+void test_1() {
     size_t size = 5;
     int* parray = malloc(size * size * sizeof(int));
     if (!parray) {
         perror("memory allocation");
         exit(1);
     }
-    print_matrix_int(parray, 5, 5);
     fill_spiral_matrix(parray, 5);
     print_matrix_int(parray, 5, 5);
     memset(parray, 0, size * size * sizeof(int));
@@ -23,5 +26,11 @@ int main()
     fill_diagonal_matrix(parray, 5);
     print_matrix_int(parray, size, size);
     free(parray);
+}
+
+int main()
+{
+    test_print_with_negatievs();
+    test_1();
     return 0;
 }
--- eltex_tasks/source/print_matrix.c
+++ eltex_tasks/source/print_matrix.c
@@ -16,7 +16,7 @@ int max_abs_element(int* parray, size_t width, size_t height) {
     return parray[max_i];
 }
 
-size_t get_number_length(int a) {
+size_t number_length(int a) {
     if (0 == a) {
         return 1;
     }
@@ -28,9 +28,20 @@ size_t get_number_length(int a) {
     }
 }
 
+size_t max_element_length(int* parray, size_t size) {
+    size_t res = 1;
+    for (size_t i = 0; i < size; ++i) {
+        size_t length = number_length(parray[i]);
+        if (length > res) {
+            res = length;
+        }
+    }
+    return res;
+}
+
 void print_matrix_int(int* parray, size_t width, size_t height) {
 
-    size_t max_number_length = get_number_length(max_abs_element(parray, width, height)), num_length = 0;
+    size_t max_number_length = max_element_length(parray, width * height), num_length = 0;
     for (size_t i = 0; i < height; ++i) {
         for (size_t j = 0; j < width; ++j) {
 
